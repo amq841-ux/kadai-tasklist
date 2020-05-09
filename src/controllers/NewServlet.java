f2e15f4f1d7d0a9802d10f2aea0be2da027fdcfb
nana <ncnana.608738752000b@gmail.com>
2020-05-09 10:56:41 +0900

    Modify Index/NewServlet

--- src/controllers/NewServlet.java
+++ src/controllers/NewServlet.java
@@ -1,17 +1,13 @@
 package controllers;
 
 import java.io.IOException;
-import java.sql.Timestamp;
 
-import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import models.Task;
-import utils.DBUtil;
 
 @WebServlet("/new")
 public class NewServlet extends HttpServlet {
@@ -24,32 +20,12 @@ public class NewServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
-        EntityManager em = DBUtil.createEntityManager();
-        em.getTransaction().begin();
+        // CSRF対策
+        request.setAttribute("_token", request.getSession().getId());
 
-        Task t = new Task();
+        // インスタンス作成 request.setAttribute("task", new Task());
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/new.jsp");
+        rd.forward(request, response);
 
-        String content = "hello";
-        t.setContent(content);
-
-        Timestamp currentTime = new Timestamp(System.currentTimeMillis());
-        t.setCreated_at(currentTime);
-        t.setUpdated_at(currentTime);
-
-        em.persist(t);
-        em.getTransaction().commit();
-
-        response.getWriter().append(Integer.valueOf(t.getId()).toString());
-
-        /*
-         * // CSRF対策 request.setAttribute("_token",
-         * request.getSession().getId());
-         *
-         * // インスタンス作成 request.setAttribute("task", new Task());
-         *
-         * RequestDispatcher rd =
-         * request.getRequestDispatcher("/WEB-INF/views/tasks/new.jsp");
-         * rd.forward(request, response);
-         */
     }
 }
